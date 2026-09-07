6e91939c2df109f7005acc5eb2db0a18062dca14
Tandel Dhvani Sureshbhai <23amtics360@gmail.com>
2026-09-07 11:22:46 +0530

    Delete Personal_Info.py

--- Personal_Info.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your Name? ")
-age = int(input("What is your Age? "))
-city = input("Where do you live (city)? ")
-
-print(f"Your name is {name}, {age} years old and live in {city}")

b1f8a9144d6ce35ae62cdfb000e150e6564c88df
Tandel Dhvani Sureshbhai <23amtics360@gmail.com>
2026-09-07 16:55:33 +0530

    Add files via upload

---
+++ Operators & Expressions/String_to_Integer.py
@@ -0,0 +1,5 @@
+value = input("Enter a Number: ")
+
+int_value = int(value) * 10
+
+print(f"The integer value of {value} multiplied by 10 is: {int_value}")
---
+++ Operators & Expressions/largest_number.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    largest = num1  
+else:
+    largest = num2
+
+print("The largest number is:", largest)

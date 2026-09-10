a6a3d069f080fd226bbd535e83e32fadc42d88a9
Tandel Dhvani Sureshbhai <23amtics360@gmail.com>
2026-09-10 12:13:18 +0530

    Delete Python Setup & Basics directory

--- Python Setup & Basics/Addition.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
-
-print(f"The sum of {num} and {num2} is {num + num2}")
--- Python Setup & Basics/Age_calculator.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-bd = int(input("What is your Birth Date ? "))
-bm = int(input("What is your Birth Month ? "))
-by = int(input("What is your Birth Year ? "))
-
-today = date.today()
-age = today.year - by - ((today.month, today.day) < (bm, bd))
-print(f"Your age is {age}")
